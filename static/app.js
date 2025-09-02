0a86e2a251ba699298fccacc9079e8c552df44cd
TONY65391 <tonyanthony65391@gmail.com>
2025-09-02 04:01:24 +0100

    04:01

--- static/app.js
+++ static/app.js
@@ -39,4 +39,77 @@ else if(body.classList.contains('projectsPage')){
     setTimeout(() => {
         sections[0].classList.add('reset');
     }, 1000);
-}
+
+    const all = document.getElementById('all');
+    const school = document.getElementById('school');
+    const cafeteria = document.getElementById('cafeteria');
+    const restaurant = document.getElementById('restaurant');
+    const golf = document.getElementById('golf');
+    const library = document.getElementById('library');
+    const projects = sections[1].querySelectorAll('.projects .project');
+    const filters = [...sections[1].querySelectorAll('.filters button')];
+    
+    function resetFilters(){
+        filters.forEach(filter => {
+            filter.classList.remove('active');
+        });
+    };
+
+    filters.map((filter, index) => {
+        filter.addEventListener('click', () => {
+            resetFilters();
+            filters[index].classList.add('active');
+        });
+    });
+
+    school.onclick = () => {
+        projects.forEach(project => {
+            project.style.display = "";
+            const attribute = project.getAttribute('project');
+            if (attribute != 'school'){
+                project.style.display = 'none';
+            };
+        });
+    };
+    cafeteria.onclick = () => {
+        projects.forEach(project => {
+            project.style.display = "";
+            const attribute = project.getAttribute('project');
+            if (attribute != 'cafeteria'){
+                project.style.display = 'none';
+            };
+        });
+    };
+    golf.onclick = () => {
+        projects.forEach(project => {
+            project.style.display = "";
+            const attribute = project.getAttribute('project');
+            if (attribute != 'golf'){
+                project.style.display = 'none';
+            };
+        });
+    };
+    restaurant.onclick = () => {
+        projects.forEach(project => {
+            project.style.display = "";
+            const attribute = project.getAttribute('project');
+            if (attribute != 'restaurant'){
+                project.style.display = 'none';
+            };
+        });
+    };
+    library.onclick = () => {
+        projects.forEach(project => {
+            project.style.display = "";
+            const attribute = project.getAttribute('project');
+            if (attribute != 'library'){
+                project.style.display = 'none';
+            };
+        });
+    };
+    all.onclick = () => {
+        projects.forEach(project => {
+            project.style.display = "";
+        });
+    };
+};
